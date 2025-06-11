a6d29afc5ce094f07fd71b94afb3a55aeb3f6cc1
ButionDev <bution.dev@gmail.com>
2025-06-11 03:18:20 +0000

    //Realizada uma atualização no código onde foi adicionado a movimentação do bispo. //Foi adicionado comentários para deixar as lógicas confusas mais fácil de entender //Bruno Bution Dos Santos, Estácio.

--- xadrez_mestre.c
+++ xadrez_mestre.c
@@ -21,17 +21,35 @@ void movercavalo (int n){
     }
 }
 
-int main(){
-    printf("***Desafio nível Mestre de xadrez***\n");
 
+//O loop de fora movimenta o bispo para a esquerda, e o interno, movimenta para direita.
+//O primeiro loop imprime e passa para o loop interno, que só vai imprimir se "n" == "m".
+//Ele executa o externo, e passa pro interno, dai decrementa "m" e não deixa o interno se repetir.
+//Após o loop interno não conseguir ser executado ele recomeça o externo com n - 1.
+void moverbispo (int n, int m){
+    for (n; n > 0; n--){
+       printf("Bispo: cima - ");
+       for (m; m == n; m--){
+            printf("Direita\n");
+       }
+    }
+}
+
+int main(){
     //loop simples para mover a torre
     movertorre(5);
     printf("\n");
+
     //tambem um loop simples para a raínha
     moverrainha(8);
     printf("\n");
+
     //alem do loop, tem um operador ternário verificar "n" e movimentar o cavalo
     movercavalo(3);
+    printf("\n");
+
+    //Função para mover o bispo usando um loop aninhado um pouco complexo
+    moverbispo(7, 7);
 
     return 0;
 }
